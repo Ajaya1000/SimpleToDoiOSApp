2fe0a92cf35ceed81f52c1267e74cf49f86731d7
Ajaya Mati <ajaya.mati@nobroker.in>
2022-06-25 10:17:54 +0530

    Table Header View

---
+++ ToDo App/Views/Components/TableHeader/TableHeaderView.swift
@@ -0,0 +1,16 @@
+//
+//  TableHeaderView.swift
+//  ToDo App
+//
+//  Created by Ajaya Mati on 24/06/22.
+//
+
+import UIKit
+
+class TableHeaderView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
